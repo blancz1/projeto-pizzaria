55658ae5ada3176eef6a9a8b1d4629eb00562aa1
blancz1 <adriano_lock@hotmail.com>
2024-07-06 14:58:42 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let show = true;
+
+const menuContent = document.querySelector('.content');
+const menuToggle = menuContent.querySelector('.menu-toggle');
+
+menuToggle.addEventListener('click', () => {
+    menuContent.classList.toggle('on', show);
+    show= !show;
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    const imageContainer = document.querySelector('.local');
+    const expandableImage = document.querySelector('.expandable-image');
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+
+    expandableImage.addEventListener('click', function () {
+        imageContainer.classList.toggle('expanded');
+        overlay.classList.toggle('show');
+    });
+
+    overlay.addEventListener('click', function () {
+        imageContainer.classList.remove('expanded');
+        overlay.classList.remove('show');
+    });
+});
